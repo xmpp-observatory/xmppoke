06ddb6d74b7fdfcd1f9a2f1c761eaa6f92d1e48a
Thijs Alkemade <me@thijsalkema.de>
2015-10-20 08:15:00 +0200

    Managed to save this file from Apollo.

--- import_directory.lua
+++ import_directory.lua
@@ -2,8 +2,12 @@ local lxp = require "lxp";
 lxp.lom = require "lxp.lom";
 local dbi = require "DBI";
 
-local dbh = assert(dbi.Connect("PostgreSQL", "xmppoke", "xmppoke", "xmppoke", "localhost", 5433));
+local dbh = assert(dbi.Connect("PostgreSQL", "xmppoke", "xmppoke", "xmppoke", "localhost", 5432));
 local stm = assert(dbh:prepare("SET TIMEZONE = 'UTC';"));
+assert(stm:execute());
+
+stm = assert(dbh:prepare("DELETE FROM public_servers;"));
+
 assert(stm:execute());
 
 for _,file in ipairs(arg) do
@@ -33,6 +37,26 @@ for _,file in ipairs(arg) do
 		return result;
 	end
 
+    local function render_attributes(t)
+        local result = "";
+        print(type(t), t);
+        for k,v in ipairs(t.attr) do
+                result = " " .. v .. "='" .. t.attr[v] .. "'"
+        end
+        return result;
+    end
+
+    local function render(t)
+        if type(t) == "string" then
+            return t;
+        end
+        local result = "<" .. t.tag .. render_attributes(t) .. ">"
+        for k, v in ipairs(t) do
+            result = result .. render(v);
+        end
+        return result .. "</" .. t.tag .. ">"
+    end
+
 	local fn = get_child(vcard, "fn");
 	local name = get_child(fn, "text");
 
@@ -45,9 +69,23 @@ for _,file in ipairs(arg) do
 	local url = get_child(vcard, "url");
 	local uri = get_child(url, "uri");
 
-	local stm = assert(dbh:prepare("INSERT INTO public_servers (server_name, founded, country, url) VALUES (?, ?, ?, ?);"));
+    local note = get_child(vcard, "note");
+    local text = note and get_child(note, "text");
 
-	assert(stm:execute(get_text(name), get_text(date), get_text(country), get_text(uri)));
+    local impp = get_child(vcard, "impp");
+    local impp_uri = impp and get_child(impp, "uri");
+
+    local vcard_rest = "";
+
+    for k, v in ipairs(vcard) do
+        if not (v.tag == "fn" or v.tag == "bday" or v.tag == "adr" or v.tag == "url" or v.tag == "note" or v.tag == "impp" or v.tag == "name" or v.tag == "ca") then
+            vcard_rest = vcard_rest .. render(v)
+        end
+    end
+
+    local stm = assert(dbh:prepare("INSERT INTO public_servers (server_name, founded, country, url, description, admin, vcard_rest) VALUES (?, ?, ?, ?, ?, ?, ?);"));
+
+	assert(stm:execute(get_text(name), get_text(date), get_text(country), get_text(uri), text and get_text(text) or nil, impp_uri and get_text(impp_uri), vcard_rest));
 end
 
 dbh:commit();
